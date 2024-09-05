351229a84f957d3d7eb7733128a2ccb9c396af36
nuwinda <info@flow200.com>
2024-09-06 10:51:49 +1200

    release

--- app/routes/_index/route.tsx
+++ app/routes/_index/route.tsx
@@ -21,34 +21,19 @@ export default function App() {
   return (
     <div className={styles.index}>
       <div className={styles.content}>
-        <h1 className={styles.heading}>A short heading about [your app]</h1>
+        <h1 className={styles.heading}>Shopify Hot Products</h1>
         <p className={styles.text}>
-          A tagline about [your app] that describes your value proposition.
+          Show hot product badge in product view page.
         </p>
-        {showForm && (
-          <Form className={styles.form} method="post" action="/auth/login">
-            <label className={styles.label}>
-              <span>Shop domain</span>
-              <input className={styles.input} type="text" name="shop" />
-              <span>e.g: my-shop-domain.myshopify.com</span>
-            </label>
-            <button className={styles.button} type="submit">
-              Log in
-            </button>
-          </Form>
-        )}
         <ul className={styles.list}>
           <li>
-            <strong>Product feature</strong>. Some detail about your feature and
-            its benefit to your customer.
+            <strong>Product feature</strong>. Count No of product views in frontend
           </li>
           <li>
-            <strong>Product feature</strong>. Some detail about your feature and
-            its benefit to your customer.
+            <strong>Product feature</strong>. Show No of product views in backend
           </li>
           <li>
-            <strong>Product feature</strong>. Some detail about your feature and
-            its benefit to your customer.
+            <strong>Product feature</strong>. Show Hot Product badge in product view page is it has higher views
           </li>
         </ul>
       </div>
